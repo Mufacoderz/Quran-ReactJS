2a6e769de9b7895d807fba89438c1e5b2112128f
Muhammad <mf9555759@gmail.com>
2025-03-21 11:50:07 +0800

    finish verse

--- Al-Quran-Reactjs/src/component/Verse.tsx
+++ Al-Quran-Reactjs/src/component/Verse.tsx
@@ -9,10 +9,12 @@ type VerseProps = {
 
 function Verse({verse, surah}: VerseProps) {
     return(
-        <div style={{ backgroundImage: `url(${bgImage})` }} className=" p-4 rounded-2xl">
-            <p className="font-xs text-light">📖 Your Daily Verse</p>
-            <p className="font-sm text-light font-semibold">{verse}</p>
-            <p className="font-xs text-light font-light">{surah}</p>
+        <div style={{ backgroundImage: `url(${bgImage})` }} className=" p-4 rounded-2xl bg-cover bg-no-repeat min-h-[155px] text-left flex flex-col justify-between gap-2">
+            <div>
+                <p className="text-xs text-light md:text-base">📖 Your Daily Verse</p>
+                <p className="text-sm text-light font-semibold md:text-xl">{verse}</p>
+            </div>
+            <p className="text-xs text-light font-light md:text-base">{surah}</p>
         </div>
     )
 }

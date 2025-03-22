c38650a20186dd183a076c07256902bf52573611
Muhammad <mf9555759@gmail.com>
2025-03-22 21:25:32 +0800

    debugging

--- Al-Quran-Reactjs/src/component/Surah/SurahCard.tsx
+++ Al-Quran-Reactjs/src/component/Surah/SurahCard.tsx
@@ -1,26 +1,27 @@
-import type {Surah} from "../../types/surah"
-import Number from "../Number"
+import { Link } from "react-router-dom";
+import type { Surah } from "../../types/surah";
+import Number from "../Number";
 
 type SurahCardProps = {
-    surah: Surah
+    surah: Surah;
+};
+
+function SurahCard({ surah }: SurahCardProps) {
+    return (
+        <Link to={`/surah/${surah.nomor}`}>
+            <div className="bg-dark p-4 rounded-lg flex justify-between gap-3">
+                <div>
+                    <Number nomor={surah.nomor} />
+                    <div className="text-light">
+                        <p>{surah.namaLatin}</p>
+                        <p>{surah.arti}</p>
+                        <p>{surah.tempatTurun} • {surah.jumlahAyat}</p>
+                    </div>
+                </div>
+                <p>{surah.nama}</p>
+            </div>
+        </Link>
+    );
 }
 
-function SurahCard({surah}: SurahCardProps){
-    return(
-        <link to={`/surah/${surah.nomor}`}>
-        <div className="bg-dark p-4 rounded-lg flex justify-between gap-3 ">
-            <div>
-            <Number nomor={surah.nomor} />
-            <div className="text-light">
-                <p>{surah.namaLatin}</p>
-                <p>{surah.arti}</p>
-                <p>{surah.tempatTurun} • {surah.jumlahAyat}</p>
-            </div>
-            </div>
-            <p>{surah.nama}</p>
-        </div>
-        </link>
-    )
-}
-
-export default SurahCard
+export default SurahCard;

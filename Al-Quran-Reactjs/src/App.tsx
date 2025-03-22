01fbbd2d1c6998ab1439facc5e55f2d76c98a455
Muhammad <mf9555759@gmail.com>
2025-03-22 21:05:51 +0800

    homepage

--- Al-Quran-Reactjs/src/App.tsx
+++ Al-Quran-Reactjs/src/App.tsx
@@ -1,7 +1,8 @@
 
 
 import './App.css'
-import Hero from './component/Hero'
+
+import HomePage from './pages/HomePages'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
    
-      <Hero/>
+      <HomePage/>
 
     </>
   )
---
+++ Al-Quran-Reactjs/src/pages/HomePages.tsx
@@ -0,0 +1,13 @@
+import Hero from "..//component/Hero"
+import SurahContainer from "../component/Surah/SurahContainer"
+
+function HomePage(){
+    return(
+        <>
+            <Hero/>
+            <SurahContainer/>
+        </>
+    )
+}
+
+export default HomePage
